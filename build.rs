e859ecb63a00a7f9e44111017ad85c3e2c89d863
Landry Gigant <landry.gigant@epitech.eu>
2025-06-30 16:48:19 +0200

    update build.rs to create the pkg path if it is not ready

--- build.rs
+++ build.rs
@@ -1,22 +1,41 @@
+use std::fs;
+use std::io::Write;
+use std::path::Path;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    
+    let pkg_path = Path::new("src/pkg");
+
+    // Create the directory if it doesn't exist
+    if !pkg_path.exists() {
+        fs::create_dir_all(pkg_path)?;
+    }
+
+    // Create or overwrite the mod.rs file
+    let mod_rs_path = pkg_path.join("mod.rs");
+    let mut mod_rs = fs::File::create(mod_rs_path)?;
+    writeln!(
+        mod_rs,
+        "pub mod exporter;\npub mod importer;\npub mod store;"
+    )?;
+
+    // Compile the .proto files into Rust
     tonic_build::configure()
         .build_server(true)
         .build_client(true)
         .out_dir("src/pkg")
-        //.compile(&["proto/importer.proto"], &["proto"])?;
         .compile_protos(&["proto/importer.proto"], &["proto"])?;
+
     tonic_build::configure()
         .build_server(true)
         .build_client(true)
         .out_dir("src/pkg")
-        //.compile(&["proto/exporter.proto"], &["proto"])?;
         .compile_protos(&["proto/exporter.proto"], &["proto"])?;
+
     tonic_build::configure()
         .build_server(true)
         .build_client(true)
         .out_dir("src/pkg")
-        //.compile(&["proto/storage.proto"], &["proto"])?;
         .compile_protos(&["proto/storage.proto"], &["proto"])?;
+
     Ok(())
 }
